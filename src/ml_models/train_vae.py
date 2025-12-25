vae kl bug fix

src/ml_models/train_vae.py
@@ -16,7 +16,7 @@ def vae_loss(recon_x, x, mu, logvar):
     # KL divergence term
     kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-    return recon_loss + kl_loss, recon_loss, kl_loss
+    return recon_loss + 0.3 * kl_loss, recon_loss, kl_loss
 
 
 def train_vae(